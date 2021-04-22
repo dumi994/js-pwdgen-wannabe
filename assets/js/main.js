31b38d2678daf8fcba5df55e31f116f3ffc98f0d
dumi994 <dumi.9944@gmail.com>
2021-04-22 15:29:04 +0200

    password generata

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// Chiedere all'utente il suo nome
+
+var namee = prompt("Qual'è il tuo nome?");
+console.log(namee );
+
+// Chiedere all'utente il suo cognnome
+
+var lastName = prompt("Qual'è il tuo cognome?");
+console.log(lastName);
+
+// Chiedere all'utente il suo colore preferito
+
+var favColour = prompt("Qual'è il tuo colore preferito?");
+console.log(favColour);
+
+//Password generata
+var pwGen = namee + lastName + favColour + '21';
+
+document.getElementById("namee").innerHTML = "Ciao " + namee;
+document.getElementById('lastName').innerHTML = "La tua nuova password è " + namee + lastName + favColour + "21";
+ 
